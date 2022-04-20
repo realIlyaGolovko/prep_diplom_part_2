75413b42fb9c69ffac19a66a665f9e57468d189b
realIlyaGolovko <real-golovko-ilya@yandex.ru>
2022-04-20 19:39:01 +0300

    added common pkg

---
+++ src/test/java/stellarburgers/common/ConstantsForTests.java
@@ -0,0 +1,6 @@
+package stellarburgers.common;
+
+public class ConstantsForTests {
+    public static final String SUCCESS_MSG_TRUE="true";
+    public static final String SUCCESS_MSG_FALSE="false";
+}
