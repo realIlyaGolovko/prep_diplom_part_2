b92dd5120d0ff24d124d538b09636f0e50fe052b
realIlyaGolovko <real-golovko-ilya@yandex.ru>
2022-04-20 19:44:45 +0300

    added common pkg

--- src/test/java/stellarburgers/user/UserCreatingTest.java
+++ src/test/java/stellarburgers/user/UserCreatingTest.java
@@ -5,13 +5,11 @@ import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import stellarburgers.user.User;
-import stellarburgers.user.UserClient;
-
 import static org.apache.http.HttpStatus.SC_OK;
+import static stellarburgers.common.ConstantsForTests.SUCCESS_MSG_TRUE;
 
 public class UserCreatingTest {
-    String expectedResult="true";
+
     User user=User.getRandomUser();
     UserClient userClient;
     String token;
@@ -27,12 +25,13 @@ public class UserCreatingTest {
     @Test
     @DisplayName("Проверка создания уникального пользователя")
     public void UserСanBeCreatedWithValidParameters(){
+        //выполнили запрос
         ValidatableResponse validatableResponse= userClient.create(user);
         String actualResult=validatableResponse.extract().jsonPath().getString("success");
         token= userClient.getAccessToken(validatableResponse);
         //Assert
         validatableResponse.assertThat().statusCode(SC_OK);
-        Assert.assertEquals("Incorrect creation status ",expectedResult,actualResult);
+        Assert.assertEquals("Incorrect creation status ",SUCCESS_MSG_TRUE,actualResult);
 
     }
 
